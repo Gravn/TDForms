4c5328d0411718d6082441cbdb280f070319473a
Mads Ludvigsen <Imy@live.dk>
2014-12-10 03:00:09 +0100

    Enemy start

--- TowerDefenseForms/TowerDefenseForms/Enemy.cs
+++ TowerDefenseForms/TowerDefenseForms/Enemy.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TowerDefenseForms
 {
-    class Enemy
+    abstract class Enemy : GameObject
     {
+        Enemy(float speed, int hp, int armor, Point[] path, int prize, PointF startPos, PointF[] shape, Color color)
+            : base(startPos, shape, color)
+        {
+
+        }
     }
 }
